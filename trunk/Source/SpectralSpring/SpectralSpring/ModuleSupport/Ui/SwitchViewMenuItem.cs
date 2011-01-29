b75528a8c60a42e80ba37e7c35ba52d49fef6ff0
bogdan.costea@gridpulse.com <bogdan.costea@gridpulse.com@78b5e931-ed29-0d49-f196-35a0e1b6379d>
2011-01-29 00:21:50 +0000

    First version, needs more work, testing and some documentation. Second sample module not done. Also, needs some more comments :)

---
+++ trunk/Source/SpectralSpring/SpectralSpring/ModuleSupport/Ui/SwitchViewMenuItem.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Windows.Media;
+
+using Microsoft.Windows.Controls.Ribbon;
+using Microsoft.Practices.Prism.Regions;
+using Spring.Context.Support;
+
+using SpectralSpring.Utils;
+
+namespace SpectralSpring.ModuleSupport.Ui
+{
+    // FIXME: explain this
+    public class SwitchViewMenuItem : RibbonApplicationMenuItem
+    {
+        private ModuleView view;
+
+        public SwitchViewMenuItem(string header, string keyTip, ImageSource iconImageSource, ModuleView viewToActivate)
+        {
+            ImageSource = iconImageSource;
+            Header = header;
+            KeyTip = keyTip;
+
+            this.view = viewToActivate;
+        }
+
+        public SwitchViewMenuItem(string id, ModuleView viewToActivate)
+        {
+            ImageSource = ResourceUtils.GetImageSourceById(id + "Icon");
+            Header = ResourceUtils.GetMessage(id + "MenuHeader");
+
+            this.view = viewToActivate;
+        }
+
+        protected override void OnClick()
+        {
+            IRegionManager regionManager = (IRegionManager) ContextRegistry.GetContext().GetObject("IRegionManager");
+            ModuleView loadedView = (ModuleView)regionManager.Regions["MainRegion"].GetView(view.ViewName);
+
+            if (loadedView != null)
+            {
+                foreach (object viewObject in regionManager.Regions["MainRegion"].Views)
+                {
+                    regionManager.Regions["MainRegion"].Deactivate(viewObject);
+                }
+
+                regionManager.Regions["MainRegion"].Activate(loadedView);
+                loadedView.Refresh();
+            }
+            else
+            {
+                regionManager.Regions["MainRegion"].Add(view, view.ViewName);
+                regionManager.Regions["MainRegion"].Activate(view);
+                view.Refresh();
+            }
+        }
+
+    }
+}
